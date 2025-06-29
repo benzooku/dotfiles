32fb0289ae4fc3b7e1883c492590672aa62693d8
benzooku <benzooku@gmail.com>
2025-06-29 11:39:47 +0200

    remove old nvim config

--- private_dot_config/nvim/lua/ben/remap.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set({"n", "v"}, "<C-c>", "\"*y")
-vim.keymap.set({"n", "v"}, "<C-v>", "\"*p")
-
--- private_dot_config/nvim/lua/configs/colors.lua
+++
@@ -1,8 +0,0 @@
-function ColorMyPencils(color)
-
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-    vim.api.nvim_set_hl(0, "NormFloat", {bg = "none"})
-    vim.api.nvim_set_hl(0, "MatchParen", {bg = "#3d375e", fg = "#edecee"})
-end
-
-ColorMyPencils()
